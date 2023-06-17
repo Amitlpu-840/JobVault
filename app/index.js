a7e4a53a977110bc0f7cf1e6b0e33a03e57a7810
Amit Giri <thisisag840@gmail.com>
2023-06-17 23:51:46 +0530

    inital setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+function Home() {
+  return (
+    <View>
+        <Text>
+            Hello world lets do it
+        </Text>
+    </View>
+  )
+}
+
+export default Home
